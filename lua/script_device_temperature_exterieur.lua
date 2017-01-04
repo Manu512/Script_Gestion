aa58bf9bf7c3e72319ce8f34c1d9e1a08e7f6b8a
Manu512 <emmanuel.waldner@gmail.com>
2017-01-04 18:40:43 +0100

    Debut du script de la gestion de temperature

---
+++ lua/script_device_temperature_exterieur.lua
@@ -0,0 +1,12 @@
+-- script domoticz/scripts/lua/script_device_temperature_exterieur.lua
+local script_label = "Temperature exterieur"
+
+local temp = "Terrasse"
+
+commandArray = {} 
+
+if devicechanged[temp] then
+	
+end
+	
+return commandArray 
